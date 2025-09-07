54deae1db3edbc2058fbcb11213514acf8cfd12c
Iampundalik <123832230+Iampundalik@users.noreply.github.com>
2025-09-07 13:15:09 +0530

    Fix string formatting for log file name

--- utils/logger.py
+++ utils/logger.py
@@ -14,7 +14,7 @@ def get_logger(name: str) -> logging.Logger:
 
     reports_dir = os.path.join(os.getcwd(), 'reports')
     os.makedirs(reports_dir, exist_ok=True)
-    log_file = os.path.join(reports_dir, f'test_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log')
+    log_file = os.path.join(reports_dir, f'test_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log')
     fh = logging.FileHandler(log_file)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
